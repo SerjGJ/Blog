9bf2f0e5b44e9fa082f8ad4f76226e4fc78b7062
Sergey Kozochkin <kozochkin2000@icloud.com>
2024-02-21 21:08:38 +0500

    Реализовать страницу Статья

--- my-app/src/components/header/components/control-panel/contol-panel.js
+++ my-app/src/components/header/components/control-panel/contol-panel.js
@@ -24,6 +24,11 @@ const ControlPanelContainer = ({ className }) => {
 	const login = useSelector(selectUserLogin);
 	const session = useSelector(selectUserSession);
 
+	const onLogout = () => {
+		dispatch(logout(session));
+		sessionStorage.removeItem('userData');
+	};
+
 	return (
 		<div className={className}>
 			<RightAligned>
@@ -35,7 +40,7 @@ const ControlPanelContainer = ({ className }) => {
 					<>
 						<UserName>{login}</UserName>
 
-						<Icon id="fa-sign-out" margin="0 0 0 10px" onClick={() => dispatch(logout(session))} />
+						<Icon id="fa-sign-out" margin="0 0 0 10px" onClick={onLogout} />
 					</>
 				)}
 			</RightAligned>
